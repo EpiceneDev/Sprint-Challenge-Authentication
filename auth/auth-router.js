f6eec01a386842aa63cb99b86fddf2a370e6846c
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-15 09:04:10 -0800

    users folder and files created

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,47 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
-  // implement registration
+const Users = require("../users/users-model.js");
+
+router.post('/api/register', (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let {username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // 2: produce a token
+        const token = getJwtToken(user.username);
+
+        // 3: send the token to the client
+        res.status(200).json({
+          message: `Welcome ${user.username}! have a token...`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  
 });
 
 module.exports = router;
